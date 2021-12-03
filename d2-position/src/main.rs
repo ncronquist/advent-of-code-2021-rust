5e5a5c253d959545d92444094864c48eec39f26b
Nicholas Cronquist <nick@ncronquist.com>
2021-12-02 23:20:23 -0500

    Add day 2 part 2 with file reading

--- d2-position/src/main.rs
+++ d2-position/src/main.rs
@@ -1,8 +1,25 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "movements.txt";
+    let movements = read_file_lines_to_vec(filename);
+
+    let position = calculate_position(&movements);
+    println!("Position: {}", position);
+
+    let better_position = calculate_better_position(&movements);
+    println!("Better Position: {}", better_position);
 }
 
-fn calculate_position(movements: &Vec<&str>) -> i32 {
+fn read_file_lines_to_vec(filename: &str) -> Vec<String> {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let r: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
+    return r;
+}
+
+fn calculate_position(movements: &Vec<String>) -> i32 {
     let (vertical, horizontal) = movements
         .iter()
         .map(|movement| {
@@ -24,11 +41,33 @@ fn calculate_position(movements: &Vec<&str>) -> i32 {
             new_pos
         });
 
-    println!("VER: {}, HOR: {}", vertical, horizontal);
     return vertical * horizontal;
 }
 
+fn calculate_better_position(movements: &Vec<String>) -> i32 {
+    let (_aim, vertical, horizontal) = movements
+        .iter()
+        .map(|movement| {
+            let mov_parts: Vec<&str> = movement.split(" ").collect();
+            let direction = mov_parts[0];
+            let distance = mov_parts[1].parse::<i32>().unwrap();
+            (direction, distance)
+        })
+        .fold((0, 0, 0), |pos, (direction, distance)| {
+            let (aim, vertical, horizontal) = pos;
 
+            let new_pos = match direction {
+                "down" => (aim + distance, vertical, horizontal),
+                "up" => (aim - distance, vertical, horizontal),
+                "forward" => (aim, vertical + (aim * distance), horizontal + distance),
+                _ => panic!("Unknown direction: {}", direction),
+            };
+
+            new_pos
+        });
+
+    return vertical * horizontal;
+}
 
 #[cfg(test)]
 mod tests {
@@ -36,13 +75,27 @@ mod tests {
 
     #[test]
     fn position() {
-        let input = vec!["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"];
+        let input: Vec<String> = vec![
+            "forward 5".into(),
+            "down 5".into(),
+            "forward 8".into(),
+            "up 3".into(),
+            "down 8".into(),
+            "forward 2".into(),
+        ];
         assert_eq!(calculate_position(&input), 150);
     }
 
-    // #[test]
-    // fn given_windows() {
-    //     let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-    //     assert_eq!(count_windows(&input), 5);
-    // }
+    #[test]
+    fn better_position() {
+        let input: Vec<String> = vec![
+            "forward 5".into(),
+            "down 5".into(),
+            "forward 8".into(),
+            "up 3".into(),
+            "down 8".into(),
+            "forward 2".into(),
+        ];
+        assert_eq!(calculate_better_position(&input), 900);
+    }
 }
